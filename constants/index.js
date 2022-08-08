e8997bb26982d9de49082c974fe37a038298aad9
Pablo Ibarra <pabloo_ibarra98@hotmail.com>
2022-08-08 17:01:05 -0500

    initial commit

---
+++ constants/index.js
@@ -0,0 +1,3 @@
+export const SUBGRAPH_URL =
+  "https://api.thegraph.com/subgraphs/name/pabloib21/celo-nft-marketplace";
+export const MARKETPLACE_ADDRESS = "0xe39A622B08F279b71789BDE16EB70AC665Cd8fB5";
